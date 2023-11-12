23d2c7735f336bc0c1ca1b6eec41c6b1e25cda3c
devin! <dvnmccormick@gmail.com>
2023-11-11 21:58:53 -0800

    basic components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import ContactList from "./components/ContactList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ContactList />
+    </>
+  );
 }
 
 export default App;
